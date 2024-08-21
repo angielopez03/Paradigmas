53b0ebe76f59ad75718d5c46ff63e3565cfc6b10
angielopez03 <147659881+angielopez03@users.noreply.github.com>
2024-08-21 09:29:40 -0500

    Create Punto1.C

---
+++ EjerciciosCondicionales_14Agosto/Punto1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+	int e;
+	printf("Cual es su edad?"); 
+	scanf("%d", &e); //pregunta al usuario su edad y la almacena en una variable
+	
+	if(e<18) //se hace un condicional
+	{
+		printf("El usuario no es mayor de edad"); //si es menor a 18 imprime indicandole que no es mayor de edad
+	}
+		else
+		{
+			printf("El usuario es mayor de edad"); //si es mayor a 18 imprime indicandole que si
+		} 
+}

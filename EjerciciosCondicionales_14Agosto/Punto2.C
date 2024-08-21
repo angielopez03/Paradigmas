a8052d84ed0ca4e7dc75da32a24a185d79fd40f2
angielopez03 <147659881+angielopez03@users.noreply.github.com>
2024-08-21 09:31:34 -0500

    Update Punto2.C

--- EjerciciosCondicionales_14Agosto/Punto2.C
+++ EjerciciosCondicionales_14Agosto/Punto2.C
@@ -3,11 +3,11 @@
 
 int main() 
 {
-	char c[] = "rwsy12"; //se declara la variable de la contrase�a y su contenido
+	char c[] = "rwsy12"; //se declara la variable de la contrasena y su contenido
 	
-	char u[50]; //Se declara la variable en el que el usuario depositara su contrase�a
-	printf("Introduce la contrase�a: ");
-	scanf("%s", &u); //imprime y lee la contrase�a depositada por el usuario
+	char u[50]; //Se declara la variable en el que el usuario depositara su contrasena
+	printf("Introduce la contrasena: ");
+	scanf("%s", &u); //imprime y lee la contrasena depositada por el usuario
 	
 	if (strcmp(c, u) == 0) //compara los strings y si coinciden imprime un resultado u otro
 	{

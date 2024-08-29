fd63d07f1a626406b9e6ad2e4892c284863c82df
angielopez03 <147659881+angielopez03@users.noreply.github.com>
2024-08-28 21:41:25 -0500

    hanoi4.hs

--- TrabajoHaskell_30Agosto.hs/hanoi4.hs
+++ TrabajoHaskell_30Agosto.hs/hanoi4.hs
@@ -7,3 +7,9 @@ hanoi4 n from to aux1 aux2 =
         hanoi4 k aux1 to from aux2 ++             -- Mueve k discos de 'aux1' a 'to'
         hanoi (n - k) to from aux1 ++              -- Mueve los n-k discos de 'to' a 'from'
         hanoi4 k to from aux2 aux1                -- Mueve k discos de 'to' a 'from'
+
+main :: IO ()
+main = do
+    let numDiscos = 4
+    let movimientos = hanoi4 numDiscos 'A' 'D' 'B' 'C'
+    print movimientos
